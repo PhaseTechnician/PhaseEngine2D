6f78bf439ce2e3da96f85c90c214f43de1d57c93
萨古拉斯 <1448973286@qq.com>
2019-07-13 22:24:59 +0800

    add lib

---
+++ PhaseEngine2D/main.cpp
@@ -0,0 +1,43 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// 创建窗口
+	GLFWwindow* window = glfwCreateWindow(800, 600, "233", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// 载入Opengl-Glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		/*渲染部分*/
+		glClearColor(0.2f, 0.5f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
